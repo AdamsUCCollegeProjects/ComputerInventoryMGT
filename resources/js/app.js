4f96f383bca3820abc48d008b30992bec3c5c01e
adamreaksmey <adaminiature@gmail.com>
2025-06-01 17:42:23 +0700

    Add Vue.js support and update dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import { createApp } from 'vue'
+
+// Import your Vue components here
+// import ExampleComponent from './components/ExampleComponent.vue'
+
+const app = createApp({})
+
+// Register your components here
+// app.component('example-component', ExampleComponent)
+
+app.mount('#app')
