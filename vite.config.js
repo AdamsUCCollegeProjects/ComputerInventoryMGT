5429310b5dfc92f23831ffefc686000b894f3651
adamreaksmey <adaminiature@gmail.com>
2025-06-18 00:10:56 +0700

    added sass

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
+import sass from "sass";
 
 export default defineConfig({
     plugins: [
